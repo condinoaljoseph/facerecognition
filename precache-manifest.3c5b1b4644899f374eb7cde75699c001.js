68107a2302c3918a8ea7f9c7e3936c6031d2ba77
Al Joseph Condino <condinoaljoseph@gmail.com>
2020-02-20 19:42:37 +0800

    Updates

---
+++ precache-manifest.3c5b1b4644899f374eb7cde75699c001.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1787ccd2dd5c29e06f8219033907006a",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "1933688ed6fb8a3eceab",
+    "url": "/facerecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "91edec5df5b1114ace2d",
+    "url": "/facerecognition/static/css/main.68e43c1c.chunk.css"
+  },
+  {
+    "revision": "1933688ed6fb8a3eceab",
+    "url": "/facerecognition/static/js/2.76035d7b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facerecognition/static/js/2.76035d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91edec5df5b1114ace2d",
+    "url": "/facerecognition/static/js/main.69a41f38.chunk.js"
+  },
+  {
+    "revision": "bcc03e14cdb175670d20",
+    "url": "/facerecognition/static/js/runtime-main.b27e2c0d.js"
+  }
+]);
